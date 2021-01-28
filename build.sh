a5581a84ac8e7c1916ffe3632c20ae3a61b6fb2a
Andrew Thomas <itz63c@statixos.com>
2021-01-27 23:40:23 -0500

    hudson: deviceclean at the end of each build
    Change-Id: I5ee3026b106548e9614338c5e8f36817b43ab110

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ rm -rf .repo/local_manifests
 repo sync --force-sync --no-clone-bundle --current-branch --no-tags -j$(nproc --all)
 echo "--- Build setup"
 . build/envsetup.sh
-echo "--- Sanitize outdir"
 lunch statix_$DEVICE-$BUILDTYPE
-m installclean
 echo "--- Build"
 m bacon
 echo "--- Upload"
 rsync --progress -a --include "statix_$DEVICE-*-$STATIX_BUILD_TYPE.zip" --exclude "*" out/target/product/$DEVICE/ deletthiskthx@frs.sourceforge.net:/home/pfs/project/statixos/11/$DEVICE/
+echo "--- Sanitize outdir (deviceclean)"
+m deviceclean
