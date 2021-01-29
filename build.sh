80d709b5dce28d5871f2d6177a414cf302e0e966
Filippo <zzkw35@gmail.com>
2021-01-29 18:27:04 +0100

    hudson: Upload boot images and allow selected devices to build fastbootimages and recovery
    Change-Id: I68a3d223f246c7de677de35ca8517d093d1ea896

--- build.sh
+++ build.sh
@@ -7,8 +7,37 @@ echo "--- Build setup"
 lunch statix_$DEVICE-$BUILDTYPE
 echo "--- Build"
 m bacon
+
+if [[ "$FASTBOOTIMAGES" == "true" ]];
+then
+    echo "--- Build fastbootimages"
+    m updatepackage
+fi
+
 echo "--- Upload Build (OTA Server, then SourceForge)"
 rsync --progress -a --include "statix_$DEVICE-*-$STATIX_BUILD_TYPE.zip" --exclude "*" out/target/product/$DEVICE/ downloads@downloads.statixos.com:/home/downloads/11/$DEVICE
 rsync --progress -a --include "statix_$DEVICE-*-$STATIX_BUILD_TYPE.zip" --exclude "*" out/target/product/$DEVICE/ deletthiskthx@frs.sourceforge.net:/home/pfs/project/statixos/11/$DEVICE/
+
+echo "--- Upload Boot Image (OTA Server, then SourceForge)"
+mv out/target/product/$DEVICE/boot.img out/target/product/$DEVICE/boot-$DEVICE-$DATE.img
+rsync --progress -a --include "boot-$DEVICE-$DATE.img" --exclude "*" out/target/product/$DEVICE/ downloads@downloads.statixos.com:/home/downloads/11/$DEVICE/bootimages
+rsync --progress -a --include "boot-$DEVICE-$DATE.img" --exclude "*" out/target/product/$DEVICE/ deletthiskthx@frs.sourceforge.net:/home/pfs/project/statixos/11/$DEVICE/bootimages
+
+if [[ "$FASTBOOTIMAGES" == "true" ]];
+then
+    echo "--- Upload Fastbootimages (OTA Server, then SourceForge)"
+    mv out/target/product/$DEVICE/statix_$DEVICE-img-eng.buildkite-agent.zip out/target/product/$DEVICE/statix_$DEVICE-$DATE-img.zip
+    rsync --progress -a --include "statix_$DEVICE-$DATE-img.zip" --exclude "*" out/target/product/$DEVICE/ downloads@downloads.statixos.com:/home/downloads/11/$DEVICE/fastbootimages
+    rsync --progress -a --include "statix_$DEVICE-$DATE-img.zip" --exclude "*" out/target/product/$DEVICE/ deletthiskthx@frs.sourceforge.net:/home/pfs/project/statixos/11/$DEVICE/fastbootimages
+fi
+
+if [[ "$RECOVERY" == "true" ]] && [ -f "out/target/product/$DEVICE/recovery.img" ];
+then
+    echo "--- Upload Recovery (OTA Server, then SourceForge)"
+    mv out/target/product/$DEVICE/recovery.img out/target/product/$DEVICE/recovery-$DEVICE-$DATE.img
+    rsync --progress -a --include "recovery-$DEVICE-$DATE.img" --exclude "*" out/target/product/$DEVICE/ downloads@downloads.statixos.com:/home/downloads/11/$DEVICE/recoveries
+    rsync --progress -a --include "recovery-$DEVICE-$DATE.img" --exclude "*" out/target/product/$DEVICE/ deletthiskthx@frs.sourceforge.net:/home/pfs/project/statixos/11/$DEVICE/recoveries
+fi
+
 echo "--- Sanitize outdir (deviceclean)"
 m deviceclean
--- generatebuilds.py
+++ generatebuilds.py
@@ -5,6 +5,7 @@ import random
 import sys
 import uuid
 
+import json
 import yaml
 
 def main():
@@ -16,6 +17,7 @@ def main():
         if not line or line.startswith("#"):
             continue
         device, build_type, variant = line.split()
+        fastbootimages, recovery = device_info(device)
 
         pipeline['steps'].append({
             'label': '{} {} {}'.format(device, variant, today.strftime("%Y%m%d")),
@@ -25,11 +27,23 @@ def main():
                     'DEVICE': device,
                     'STATIX_BUILD_TYPE': variant,
                     'BUILDTYPE': build_type,
+                    'DATE' : today.strftime("%Y%m%d"),
+                    'FASTBOOTIMAGES': fastbootimages,
+                    'RECOVERY': recovery,
                 },
                 'branch': "${BUILDKITE_BRANCH}"
             },
         })
     print(yaml.dump(pipeline))
+
+def device_info(device):
+    open_devices = open('devices.json')
+    devices = json.load(open_devices)
+    for line in devices['official_devices']:
+        if line['codename'] == device:
+            fastbootimages = line['fastbootimages']
+            recovery = line['lineage_recovery']
+            return fastbootimages, recovery
 
 if __name__ == '__main__':
     main()
