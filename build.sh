0b2ff1ed0c2e18a6abe9074c2914d7a06321028c
Andrew Thomas <itz63c@statixos.com>
2021-02-03 18:48:13 -0500

    hudson: Upload to our new file server as well.
    Change-Id: I4662d597fc16c2b806170597484a8770037f2c6a

--- build.sh
+++ build.sh
@@ -7,7 +7,8 @@ echo "--- Build setup"
 lunch statix_$DEVICE-$BUILDTYPE
 echo "--- Build"
 m bacon
-echo "--- Upload"
+echo "--- Upload Build (OTA Server, then SourceForge)"
+rsync --progress -a --include "statix_$DEVICE-*-$STATIX_BUILD_TYPE.zip" --exclude "*" out/target/product/$DEVICE/ downloads@downloads.statixos.com:/home/downloads/11/$DEVICE
 rsync --progress -a --include "statix_$DEVICE-*-$STATIX_BUILD_TYPE.zip" --exclude "*" out/target/product/$DEVICE/ deletthiskthx@frs.sourceforge.net:/home/pfs/project/statixos/11/$DEVICE/
 echo "--- Sanitize outdir (deviceclean)"
 m deviceclean
