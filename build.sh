a0925366fad01f946459fb5c3262ac99a3e1b376
Filippo <zzkw35@gmail.com>
2021-03-05 20:20:53 +0100

    hudson: Specify build type for Boot, Recovery and Fastbootimages
    Change-Id: I7401a42aa0324fbe4fc0caa2a155839c169dd772

--- build.sh
+++ build.sh
@@ -23,24 +23,24 @@ rsync --progress -a --include "statix_$DEVICE-*-$STATIX_BUILD_TYPE.zip" --exclud
 rsync --progress -a --include "statix_$DEVICE-*-$STATIX_BUILD_TYPE.zip" --exclude "*" $OUT/ deletthiskthx@frs.sourceforge.net:/home/pfs/project/statixos/11-$STATIX_BUILD_TYPE/$DEVICE/
 
 echo "--- Upload Boot Image (OTA Server, then SourceForge)"
-mv $OUT/boot.img $OUT/boot-$DEVICE-$(date +%Y%m%d).img
-rsync --progress -a --include "boot-$DEVICE-$(date +%Y%m%d).img" --exclude "*" $OUT/ downloads@downloads.statixos.com:/home/downloads/11-$STATIX_BUILD_TYPE/$DEVICE/bootimages
-rsync --progress -a --include "boot-$DEVICE-$(date +%Y%m%d).img" --exclude "*" $OUT/ deletthiskthx@frs.sourceforge.net:/home/pfs/project/statixos/11-$STATIX_BUILD_TYPE/$DEVICE/bootimages
+mv $OUT/boot.img $OUT/boot-$DEVICE-$(date +%Y%m%d)-$STATIX_BUILD_TYPE.img
+rsync --progress -a --include "boot-$DEVICE-$(date +%Y%m%d)-$STATIX_BUILD_TYPE.img" --exclude "*" $OUT/ downloads@downloads.statixos.com:/home/downloads/11-$STATIX_BUILD_TYPE/$DEVICE/bootimages
+rsync --progress -a --include "boot-$DEVICE-$(date +%Y%m%d)-$STATIX_BUILD_TYPE.img" --exclude "*" $OUT/ deletthiskthx@frs.sourceforge.net:/home/pfs/project/statixos/11-$STATIX_BUILD_TYPE/$DEVICE/bootimages
 
 if [[ "$FASTBOOTIMAGES" == "true" ]];
 then
     echo "--- Upload Fastbootimages (OTA Server, then SourceForge)"
-    mv $OUT/statix_$DEVICE-img-eng.buildkite-agent.zip $OUT/statix_$DEVICE-$(date +%Y%m%d)-img.zip
-    rsync --progress -a --include "statix_$DEVICE-$(date +%Y%m%d)-img.zip" --exclude "*" $OUT/ downloads@downloads.statixos.com:/home/downloads/11-$STATIX_BUILD_TYPE/$DEVICE/fastbootimages
-    rsync --progress -a --include "statix_$DEVICE-$(date +%Y%m%d)-img.zip" --exclude "*" $OUT/ deletthiskthx@frs.sourceforge.net:/home/pfs/project/statixos/11-$STATIX_BUILD_TYPE/$DEVICE/fastbootimages
+    mv $OUT/statix_$DEVICE-img-eng.buildkite-agent.zip $OUT/statix_$DEVICE-$(date +%Y%m%d)-$STATIX_BUILD_TYPE-img.zip
+    rsync --progress -a --include "statix_$DEVICE-$(date +%Y%m%d)-$STATIX_BUILD_TYPE-img.zip" --exclude "*" $OUT/ downloads@downloads.statixos.com:/home/downloads/11-$STATIX_BUILD_TYPE/$DEVICE/fastbootimages
+    rsync --progress -a --include "statix_$DEVICE-$(date +%Y%m%d)-$STATIX_BUILD_TYPE-img.zip" --exclude "*" $OUT/ deletthiskthx@frs.sourceforge.net:/home/pfs/project/statixos/11-$STATIX_BUILD_TYPE/$DEVICE/fastbootimages
 fi
 
 if [[ "$RECOVERY" == "true" ]] && [ -f "$OUT/recovery.img" ];
 then
     echo "--- Upload Recovery (OTA Server, then SourceForge)"
-    mv $OUT/recovery.img $OUT/recovery-$DEVICE-$(date +%Y%m%d).img
-    rsync --progress -a --include "recovery-$DEVICE-$(date +%Y%m%d).img" --exclude "*" $OUT/ downloads@downloads.statixos.com:/home/downloads/11-$STATIX_BUILD_TYPE/$DEVICE/recoveries
-    rsync --progress -a --include "recovery-$DEVICE-$(date +%Y%m%d).img" --exclude "*" $OUT/ deletthiskthx@frs.sourceforge.net:/home/pfs/project/statixos/11-$STATIX_BUILD_TYPE/$DEVICE/recoveries
+    mv $OUT/recovery.img $OUT/recovery-$DEVICE-$(date +%Y%m%d)-$STATIX_BUILD_TYPE.img
+    rsync --progress -a --include "recovery-$DEVICE-$(date +%Y%m%d)-$STATIX_BUILD_TYPE.img" --exclude "*" $OUT/ downloads@downloads.statixos.com:/home/downloads/11-$STATIX_BUILD_TYPE/$DEVICE/recoveries
+    rsync --progress -a --include "recovery-$DEVICE-$(date +%Y%m%d)-$STATIX_BUILD_TYPE.img" --exclude "*" $OUT/ deletthiskthx@frs.sourceforge.net:/home/pfs/project/statixos/11-$STATIX_BUILD_TYPE/$DEVICE/recoveries
 fi
 
 echo "--- Sanitize outdir (deviceclean)"
